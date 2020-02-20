ae30f477f75ccfc61dd3f57ca8b59dcf430528f9
Ian Fijolek <ian@iamthefij.com>
2020-02-19 22:13:07 -0800

    Add ability to customize metrics port

--- main.go
+++ main.go
@@ -87,6 +87,7 @@ func main() {
 	flag.BoolVar(&LogDebug, "debug", false, "Enables debug logs (default: false)")
 	flag.BoolVar(&ExportMetrics, "metrics", false, "Enables prometheus metrics exporting (default: false)")
 	flag.BoolVar(&PyCompat, "py-compat", false, "Enables support for legacy Python Minitor config. Will eventually be removed. (default: false)")
+	flag.IntVar(&MetricsPort, "metrics-port", 8080, "The port that Prometheus metrics should be exported on, if enabled. (default: 8080)")
 	var showVersion = flag.Bool("version", false, "Display the version of minitor and exit")
 	var configPath = flag.String("config", "config.yml", "Alternate configuration path (default: config.yml)")
 	flag.Parse()
